31f05be9ead737d13847212db878f5968dc91434
Phil Cote <cotejrp@gmail.com>
2016-09-04 19:30:10 -0400

    Rebuild of the model.py file I messed up and wiped from history... this time using ORM system

---
+++ roles/svgpaint/files/model.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, Column, Text, Integer, VARCHAR, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from configparser import ConfigParser
+import os
+from flask_login import UserMixin
+
+# ENGINE SETUP
+this_directory = __file__.rsplit(os.path.sep, maxsplit=1)[0]
+config_file_path = "{}{}config.ini".format(this_directory, os.path.sep)
+cp = ConfigParser()
+cp.read(config_file_path)
+config_sec = cp["dbconfig"]
+user = config_sec.get("username")
+pw = config_sec.get("password")
+db_url = "mysql+pymysql://{}:{}@localhost/svgpaint".format(user, pw)
+eng = create_engine(db_url)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    username = Column(Text, primary_key=True, nullable=False)
+    password = Column(Text, nullable=False)
+    is_active = Column(Boolean, default=False)
+    is_authenticated = Column(Boolean, default=False)
+    is_anonymous = Column(Boolean, default=False)
+
+    def get_id(self):
+        return self.username
+
+Session = sessionmaker(bind=eng)
+Base.metadata.create_all(eng)
+
+
+if __name__ == '__main__':
+    sess = Session()
+    user = User(username="cotejrp", password="rosebud")
+    sess.add(user)
+    sess.commit()
