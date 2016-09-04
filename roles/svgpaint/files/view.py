e9be43d291762a3cf832ab27e111a6256723bbf2
Phil Cote <cotejrp@gmail.com>
2016-09-04 12:52:15 -0400

    Login page with flask-login based authentication put in place.

--- roles/svgpaint/files/view.py
+++ roles/svgpaint/files/view.py
@@ -1,6 +1,32 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, render_template
+from flask_login import LoginManager, login_user
+import model
+
 
 app = Flask(__name__)
+logman = LoginManager()
+logman.init_app(app)
+
+@logman.user_loader
+def load_user(user_id):
+    user = model.get_user(user_id)
+    return user
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("usernameTF")
+    password = request.form.get("passwordTF")
+    password_correct = model.is_password_valid(username, password)
+
+    if password_correct:
+        user = model.get_user(username)
+        user.is_active = True
+        user.is_authenticated = True
+        login_user(user)
+        return render_template("mainpage.html")
+    else:
+        return "Your credentials are wrong. Please backspace and try again"
+
 
 @app.route("/")
 def index():
@@ -20,4 +46,5 @@ def new_image():
     return "Stub stuff for server side new image creation..."
 
 if __name__ == '__main__':
+    app.secret_key="SetSecretKeyHere"
     app.run(debug=True)
