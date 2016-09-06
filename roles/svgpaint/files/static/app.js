87815391c76e71e8303b9e113ecb79c9ca6da93c
Phil Cote <cotejrp@gmail.com>
2016-09-06 14:12:46 -0400

    Demarcation of drawing routines to be kept separate from saving and loading pics.

--- roles/svgpaint/files/static/app.js
+++ roles/svgpaint/files/static/app.js
@@ -5,6 +5,7 @@ $(function(){
     var brushSize = 10;
     var currentColor = "#000000";
 
+    /////////////// Routines for drawing in the drawing area ///////////////
     var mouseUpHandler = function(evt){
         dragging = false;
     };
@@ -49,4 +50,7 @@ $(function(){
     $("#brushSizeField").change(brushChangeHandler);
     $("#colorInput").change(colorChangeHandler);
     $("#wipeButton").click(wipeClickHandler);
+
+    ///////////////// end routines for drawing in the drawing area ////////////
+
 });
